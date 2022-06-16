c03a6458e5d2c415a86b21885c5b6ed76ff58867
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-06-16 14:02:14 +0100

    fix: button hrefs

--- components/navbar.js
+++ components/navbar.js
@@ -98,7 +98,7 @@ const Navbar = (props) => {
           </LinkItem>
           <LinkItem
             target="_blank"
-            href="https://github.com/craftzdog/craftzdog-homepage"
+            href="https://github.com/HediKhemiri3001/portfolio"
             path={path}
             display="inline-flex"
             alignItems="center"
@@ -129,7 +129,7 @@ const Navbar = (props) => {
                 </NextLink>
                 <MenuItem
                   as={Link}
-                  href="https://github.com/craftzdog/craftzdog-homepage"
+                  href="https://github.com/HediKhemiri3001/portfolio"
                 >
                   View Source
                 </MenuItem>
